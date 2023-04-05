150463af4abdae691ef4b20aa2a8ce0767b64e60
ChungNguyenVr912 <126665381+ChungNguyenVr912@users.noreply.github.com>
2023-04-05 14:31:44 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,12 @@
+import java.util.Date;
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
+        Date date = new Date();
+        System.out.println(date.getTime());
 //        Scanner scanner = new Scanner(System.in);
 //        int action;
 //        int hp = 100;
